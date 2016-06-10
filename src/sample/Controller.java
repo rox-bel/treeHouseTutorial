d858259c7196f6438abd12d85e508b2682e6ea12
rox-bel <robel_belay@hotmail.com>
2016-06-10 08:00:47 -0400

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
+    //edited in gethub
     @FXML private TextField firstName;
     public void handleSaySup(ActionEvent actionEvent) {
         System.out.printf("sup %s %n",firstName.getText());
